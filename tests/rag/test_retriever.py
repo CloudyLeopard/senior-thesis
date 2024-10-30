d0490e4be897912f51e5ab136c32c27f9240b068
Daniel Liu <rizhendan@gmail.com>
2024-10-29 23:33:03 -0400

    hatch build

--- tests/rag/test_retriever.py
+++ tests/rag/test_retriever.py
@@ -1,6 +1,6 @@
 import pytest
 
-from rag.retrievers import *
+from rag.retrievers import SimpleRetriever
 from rag.models import Document
 
 @pytest.fixture(
--- tests/rag/test_sources.py
+++ tests/rag/test_sources.py
@@ -1,7 +1,7 @@
 import pytest
 import pytest_asyncio
 
-from rag.sources import *
+from rag.sources import GoogleSearchData, DirectoryData
 from rag.models import Document
 
 @pytest.fixture(params=[
