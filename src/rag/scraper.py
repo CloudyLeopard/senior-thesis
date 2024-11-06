d74f295b012b16e2efbf47a0287a9161f5765b89
Daniel Liu <rizhendan@gmail.com>
2024-11-06 15:16:20 -0500

    scraper update: accept client as input, accept customizable html parser

--- src/rag/scraper.py
+++ src/rag/scraper.py
@@ -8,55 +8,81 @@ from typing import List, Dict
 
 # TODO: timeout error
 
+
 class WebScraper:
     """Scrapes list of websites using aiohttp or requests, and fallsback to selenium if aiohttp fails"""
 
-    def __init__(self, headless=True):
+    def __init__(
+        self,
+        headless=True,
+        html_parser: callable = None,
+        sync_client: httpx.AsyncClient = None,
+        async_client: httpx.AsyncClient = None,
+    ):
         self.headless = headless
-    
+        self.html_parser = html_parser or self.default_html_parser
+        self.sync_client = sync_client
+        self.async_client = async_client
+
     @staticmethod
-    def _scrape_html(html: str) -> Dict[str, str]:
+    def default_html_parser(html: str) -> Dict[str, str]:
         """Scrape html and return a dict with different information scraped from the site"""
 
         # parse html
         soup = BeautifulSoup(html, "lxml")
 
         # scrape title
-        
-        title = soup.title.get_text(" ", strip=True) if soup.title else ""
+        if soup.title:
+            title = soup.title.get_text(" ", strip=True)
 
         # scrape article content
         text = "\n".join([p.get_text(" ", strip=True) for p in soup.find_all("p")])
 
-        # ...
+        # time
+        time = soup.find("time")
+        if time:
+            # get the "datetime" from the time attribute.
+            # if DNE, fall back to just the text shown
+            time = time.get("datetime") or time.get_text()
 
         # putting all the scraped information into a dict
-        scraped_data = {
-            "title": title,
-            "content": text
-        }
+        scraped_data = {"title": title or "", "content": text, "time": time or ""}
 
         return scraped_data
-    
+
+    def set_html_parser(self, html_parser: callable = None):
+        """
+        Set a custom HTML parser for the WebScraper instance.
+
+        Args:
+            html_parser (callable, optional): A callable that takes an HTML string
+            and returns a dictionary with parsed information. If None, the default
+            HTML parser will be used.
+        """
+        if html_parser is not None:
+            self.html_parser = html_parser
+        else:
+            self.html_parser = self.default_html_parser
+
     def _create_selenium_driver(self):
         """Create and configure the Selenium WebDriver."""
         chrome_options = webdriver.ChromeOptions()
         if self.headless:
-            chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
 
         # should not need to set driver instance for Selenium version >= 4.6
         chrome_service = webdriver.ChromeService()
 
         driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-        
+
         return driver
 
     def _scrape_with_selenium(self, driver, url: str):
         print(url)
         """Fallback to Selenium to scrape the page."""
-        
+
         try:
             driver.get(url)
             time.sleep(2)  # Give the page time to load
@@ -65,81 +91,149 @@ class WebScraper:
         except Exception as e:
             print(f"Error occurred while scraping {url} with Selenium: {e}")
             return None
-    
-    
-    async def async_scrape_links(self, links: List[str]) -> List[Dict[str, str]] | None:
-        """Scrape multiple links and returns a list of dicts (scraped info). Returns None if neither method works."""
-        async def _scrape_link(client: httpx.AsyncClient, driver, url: str) -> Dict[str, str]:
-            """internal helper function for scraping a single link"""
-            r = await client.get(url)
-            if (r.headers.get("Content-Type") == "application/pdf"):
-                # if the link is a pdf, return None
-                return None
-            try:
-                r.raise_for_status()
-                html = r.text
-                return self._scrape_html(html)
-            except httpx.HTTPError as exc:
-                print(f"Error while requesting {exc.request.url!r}.")
-                print(f"Falling back to Selenium for {url}")
-                html = self._scrape_with_selenium(driver, url)
-                return self._scrape_html(html)
-        
-        driver = self._create_selenium_driver()
-        try:
-            async with httpx.AsyncClient() as client:
-                tasks = [_scrape_link(client, driver, link) for link in links]
-                return await asyncio.gather(*tasks)
-        finally:
-            driver.quit()
-            
 
-    
-    def scrape_links(self, links: List[str]) -> List[Dict[str, str]]:
-        """Scrape multiple links and returns a list of dicts (scraped info). Returns None if neither method works."""
-        
-        def _scrape_link(client: httpx.Client, driver, url: str) -> Dict[str, str]:
-            """Try requests, fallback to Selenium if requests fail."""
-            r = client.get(url)
-            if (r.headers.get("Content-Type") == "application/pdf"):
+    async def async_scrape_link(
+        self,
+        url: str,
+        driver: webdriver = None,
+        headers: Dict[str, str] = None,
+        *args,
+        **kwargs,
+    ) -> Dict[str, str] | None:
+        """Async function for scraping a single link"""
+
+        if self.async_client is None:
+            client = httpx.AsyncClient(headers=headers)
+        else:
+            client = self.async_client
+
+        r = await client.get(url)
+        try:
+            r.raise_for_status()
+            if r.headers.get("Content-Type") == "application/pdf":
                 # if the link is a pdf, return None
                 return None
-            try:
-                r.raise_for_status()
-                html = r.text
-                return self._scrape_html(html)
-            except httpx.HTTPError as exc:
+
+            html = r.text
+            return self.html_parser(html, *args, **kwargs)
+        except httpx.HTTPError as exc:
+            if driver:
                 print(f"Error while requesting {exc.request.url!r}.")
                 print(f"Falling back to Selenium for {url}")
                 html = self._scrape_with_selenium(driver, url)
-                return self._scrape_html(html)
-        
+                if html is None:
+                    return None
+                return self.html_parser(html, *args, **kwargs)
+            else:
+                print(f"Error while requesting {exc.request.url!r}.")
+                print(f"No Selenium driver provided. Skipping {url}.")
+                return None
+        finally:
+            if self.async_client is None:
+                await client.aclose()
+
+    async def async_scrape_links(
+        self, links: List[str], headers: Dict[str, str] = None
+    ) -> List[Dict[str, str]] | None:
+        """Scrape multiple links and returns a list of dicts (scraped info). Returns None if neither method works.
+
+        Args:
+            links: List of URLs to scrape
+            Additional arguments to pass to the html_parser function
+        """
+
+        driver = self._create_selenium_driver()
+        if self.async_client is None:
+            client = httpx.AsyncClient(headers=headers)
+        else:
+            client = self.async_client
+
+        try:
+            return await asyncio.gather(
+                *[self.async_scrape_link(url=link, driver=driver) for link in links]
+            )
+        finally:
+            if self.async_client is None:
+                await client.aclose()
+            driver.quit()
+
+    def scrape_link(
+        self,
+        url: str,
+        driver: webdriver = None,
+        headers: Dict[str, str] = None,
+        *args,
+        **kwargs,
+    ):
+        """Try requests, fallback to Selenium if requests fail."""
+        if self.sync_client is None:
+            client = httpx.Client(headers=headers)
+        else:
+            client = self.sync_client
+
+        r = client.get(url)
+        try:
+            r.raise_for_status()
+            if r.headers.get("Content-Type") == "application/pdf":
+                # if the link is a pdf, return None
+                return None
+
+            html = r.text
+            return self.html_parser(html, *args, **kwargs)
+        except httpx.HTTPError as exc:
+            if driver:
+                print(f"Error while requesting {exc.request.url!r}.")
+                print(f"Falling back to Selenium for {url}")
+                html = self._scrape_with_selenium(driver, url)
+                if html is None:
+                    return None
+                return self.html_parser(html, *args, **kwargs)
+            else:
+                print(f"Error while requesting {exc.request.url!r}.")
+                print(f"No Selenium driver provided. Skipping {url}.")
+                return None
+        finally:
+            if self.sync_client is None:
+                client.close()
+
+    def scrape_links(
+        self, links: List[str], headers: Dict[str, str] = None
+    ) -> List[Dict[str, str]]:
+        """Scrape multiple links and returns a list of dicts (scraped info). Returns None if neither method works."""
+
+        if self.sync_client is None:
+            client = httpx.Client(headers=headers)
+        else:
+            client = self.sync_client
+
         driver = self._create_selenium_driver()
         try:
-             with httpx.Client() as client:
-                return [_scrape_link(client, driver, link) for link in links]
+            return [self.scrape_link(url=link, driver=driver) for link in links]
         finally:
+            if self.sync_client is None:
+                client.close()
             driver.quit()
 
 
 # Example usage
 if __name__ == "__main__":
-    async def main():        
+
+    async def main():
         scraper = WebScraper()
-        
+
         # Links to scrape
         links = [
             "https://en.wikipedia.org/wiki/Cleveland_Guardians",
-            "https://python.langchain.com/docs/concepts/#prompt-templates"
+            "https://python.langchain.com/docs/concepts/#prompt-templates",
         ]
-        
+
         # Run the scraping
         scraped_data = await scraper.scrape_links(links)
-        
+
         # Output the results
         for i, data in enumerate(scraped_data):
             if data:
                 print(f"Content for {links[i]}:\n{data[:500]}...\n")
-    
+
     # Run the async event loop
     asyncio.run(main())
--- tests/rag/test_scraper.py
+++ tests/rag/test_scraper.py
@@ -1,30 +1,91 @@
 import pytest
+import pytest_asyncio
 
 from rag.scraper import WebScraper
+import httpx
+import asyncio
 
-# Arrange
+
+@pytest_asyncio.fixture(params=[True, False])
+async def scraper(request):
+    """test scraper with inputted clients and not inputted clients"""
+    if request.param:
+        sync_client = httpx.Client()
+        async_client = httpx.AsyncClient()
+    else:
+        sync_client = None
+        async_client = None
+
+    yield WebScraper(
+        sync_client = sync_client,
+        async_client = async_client
+    )
+
+    if request.param:
+        sync_client.close()
+        await async_client.aclose()
+
 @pytest.fixture
-def scraper():
-    return WebScraper()
-
-@pytest.mark.asyncio(loop_scope="session")
-async def test_scrape(scraper):
-    """test overall scraper"""
-    urls = [
+def urls():
+    return [
         "https://en.wikipedia.org/wiki/Cleveland_Guardians",
-        "https://python.langchain.com/docs/concepts/#prompt-templates"
+        "https://python.langchain.com/docs/concepts/#prompt-templates",
     ]
 
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_async_scrape_links(scraper, urls):
+    """test overall scraper"""
     # test async version
     data = await scraper.async_scrape_links(urls)
 
     assert len(urls) == len(data)
-    assert all(data) # text is not empty
+    assert all(data)  # text is not empty
 
     # test nonasync version
     data = scraper.scrape_links(urls)
     assert len(urls) == len(data)
     assert all(data)
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_async_scrape_link(scraper, urls):
+    data = await asyncio.gather(*(scraper.async_scrape_link(url) for url in urls))
+    
+    assert len(urls) == len(data)
+    assert all(data)
+
+def test_scrape_links(scraper, urls):
+    data = scraper.scrape_links(urls)
+    assert len(urls) == len(data)
+    assert all(data)
+
+def test_scrape_link(scraper, urls):
+    data = [scraper.scrape_link(url) for url in urls]
+    assert len(urls) == len(data)
+    assert all(data)
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_scrape_custom_html_parser(scraper):
+    """test custom html parser"""
+    url = "https://en.wikipedia.org/wiki/Vector_database"
+    def custom_parser(html: str, random_str: str):
+        return {"title": "some title", "text": html, "random_str": random_str}
+    
+    scraper.set_html_parser(custom_parser)
+
+    # test sync
+    data = scraper.scrape_link(url, random_str = "hahahahaha")
+    assert data["title"] == "some title"
+    assert data["random_str"] == "hahahahaha"
+    assert data["text"] and isinstance(data["text"], str) # this should contain the scraped html
+
+    # test async
+    data = await scraper.async_scrape_link(url, random_str = "hahahahaha")
+    assert data["title"] == "some title"
+    assert data["random_str"] == "hahahahaha"
+    assert data["text"] and isinstance(data["text"], str) # this should contain the scraped html
+    scraper.set_html_parser() # reset scraper to default
+
 
 def test_scrape_selenium(scraper):
     """test scraping with selenium"""
